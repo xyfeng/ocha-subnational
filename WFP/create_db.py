8b6ae91d0bbbeeff2eee00f16c3da5b908dec6b1
xyfeng <fxy0111@gmail.com>
2014-07-25 08:26:41 -0400

    database added

--- WFP/create_db.py
+++ WFP/create_db.py
@@ -8,9 +8,12 @@ connection.text_factory = str
 cursor = connection.cursor()
 
 # Create the table
-# cursor.execute('DROP TABLE IF EXISTS wfp')
-# cursor.execute('CREATE TABLE wfp ( region text, admin1 text, admin2 text, period text, indID text, indicator_name text, value real, units text) ')
-# connection.commit()
+cursor.execute('DROP TABLE IF EXISTS wfp')
+cursor.execute('CREATE TABLE wfp ( indicator_name text, region_name text, admin1_name text, admin2_name text, period text, value real, region text, admin1 text, admin2 text, indID text, units text) ')
+connection.commit()
+
+# Create CSV export 
+csv_export = [['indicator_name', 'region_name', 'admin1_name', 'admin2_name', 'period', 'value', 'region', 'admin1', 'admin2', 'indID', 'units']]
 
 # Load the CSV file into CSV reader
 indicators = []
@@ -22,6 +25,7 @@ for row in creader:
 csvfile = open('value.csv', 'rb')
 creader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
+countries = json.load(open('regional.json'))
 states = json.load(open('../FAO/fao_state.json'))
 cities = json.load(open('../FAO/fao_city.json'))
 
@@ -46,21 +50,26 @@ def find_city_list(country, state):
 def find_state_code(country, name):
 	state_list = find_state_list(country)
 	for s in state_list:
-		if s['name'] == name:
-			return s['code']
-	print 'looking for: ' + name
-	for s in state_list:
-		if name in s['name']:
-			print 'found: ' + s['name']
+		if s['name'].encode('UTF-8') == name:
 			return s['code']
 	for s in state_list:
-		if s['name'] in name:
+		if name in s['name'].encode('UTF-8'):
+			print 'looking for: ' + name
 			print 'found: ' + s['name']
 			return s['code']
-	print 'NOT FOUND'
+	# for s in state_list:
+	# 	if s['name'].encode('UTF-8') in name:
+	# 		print 'found: ' + s['name']
+	# 		return s['code']
+	return 0
+def find_city_code(country, state, name):
+	city_list = find_city_list(country, state)
+	for c in city_list:
+		if c['name'].encode('UTF-8') == name:
+			return c['code']
 	return 0
 
-# print find_state_list('BDI')
+# print find_state_list('RWA')
 
 # Iterate through the CSV reader, inserting values into the database
 skiptitle = True
@@ -69,12 +78,32 @@ for row in creader:
     skiptitle = False
   else:
   	region = row[0]
+  	for one in countries:
+  		if one['alpha-3'] == region:
+  			region_name = one['name']
   	admin1 = row[1]
+  	admin1_name = 'NA'
   	if admin1 != 'NA':
+	  	# print admin1
   		code = find_state_code(region, admin1)
   		if code != 0:
   			admin1 = code
+		  	for one in states:
+		  		if one['code'] == admin1:
+		  			admin1_name = one['name']
+	  	else:
+	  		continue
   	admin2 = row[2]
+  	admin2_name = 'NA'
+  	if admin2 != 'NA':
+  		code = find_city_code(region, admin1, admin2)
+  		if code != 0:
+  			admin2 = code
+		  	for one in cities:
+		  		if one['code'] == admin2:
+		  			admin2_name = one['name']
+	  	else:
+	  		continue
   	period = row[3]
   	indID = row[5]
   	value = float(row[6])
@@ -85,9 +114,15 @@ for row in creader:
   			indicator_name = one[1]
   			units = one[2]
   			break
-#   	cursor.execute('INSERT INTO wfp VALUES (?,?,?,?,?,?,?,?)', (region, admin1, admin2, period, indID, indicator_name, value, units))
-
+  	cursor.execute('INSERT INTO wfp VALUES (?,?,?,?,?,?,?,?,?,?,?)', (indicator_name, region_name, admin1_name, admin2_name, period, value, region, admin1, admin2, indID, units))
+  	csv_export.append([indicator_name, region_name, admin1_name, admin2_name, period, value, region, admin1, admin2, indID, units])
 # Close the csv file, commit changes, and close the connection
-# csvfile.close()
-# connection.commit()
-# connection.close()
+csvfile.close()
+connection.commit()
+connection.close()
+
+
+with open('wfp_data.csv', 'wb') as csvfile:
+    dbwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
+    for one in csv_export:
+    	dbwriter.writerow(one)
